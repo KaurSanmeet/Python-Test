d8ccea32769639dfa55f0bf43430199964c3eb98
Sanmeet Kaur <130092581+KaurSanmeet@users.noreply.github.com>
2025-04-28 23:30:33 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,12 @@
 from main import add
+from main import sub
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+
+def test_sub():
+    assert sub(2, 3) == -1
+    assert sub(0, 0) == 0
